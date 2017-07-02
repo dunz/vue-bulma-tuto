d8d5e4d948c492c7713fbc79e5dc4f264c063a3f
djlee <djlee@smilegate.com>
2017-07-02 16:16:27 +0900

    project init

---
+++ src/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <div id="app">
+    <div class="nav has-shadow">
+      <div class="container">
+        <div class="nav-left">
+          <router-link to="/" class="nav-item">MyCompany</router-link>
+        </div>
+
+        <span class="nav-toggle" v-on:click="toggleNav" v-bind:class="{'is-active': isActive}">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+
+        <div class="nav-right nav-menu" v-bind:class="{'is-active': isActive}">
+          <router-link to="/" class="nav-item r-item"></router-link>
+          <router-link to="/faq" class="nav-item r-item">Features</router-link>
+          <router-link to="/faq" class="nav-item r-item">About</router-link>
+          <router-link to="/faq" class="nav-item r-item">FAQ</router-link>
+
+          <div class="nav-item">
+            <p class="control">
+              <a href="" class="button is-primary is-outlined">
+              <span class="icon">
+                <i class="fa fa-download"></i>
+              </span>
+                <span>Join Now</span>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <router-view></router-view>
+
+    <footer class="footer is-primary">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <p>And this right here is a spiffy footer, where you can put stuff.</p>
+          </div>
+          <div class="column has-text-right">
+            <a href="#" class="icon"><i class="fa fa-facebook"></i></a>
+            <a href="#" class="icon"><i class="fa fa-twitter"></i></a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data: () => ({
+      isActive: false
+    }),
+    methods: {
+      toggleNav() {
+        this.isActive = !this.isActive;
+      }
+    }
+  }
+</script>
+
+<style lang="sass">
+  @import '../node_modules/bulma/bulma.sass'
+  @import './mq'
+
+  .nav
+    background-color: #383838
+    a:hover
+      color: gray
+
+  .nav-left a
+    color: #fff
+    font-weight: bold
+
+  a.r-item
+    color: #c1c1c1
+    padding: 0.5rem 1.75rem
+    +mobile
+      color: gray
+      &:hover
+        background-color: #f1f1f1
+
+  .nav-toggle span
+    background-color: #c1c1c1
+
+  footer
+    background-color: $primary !important
+    color: #fff
+
+    .icon
+      color: #fff
+      margin-left: 20px
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="home">
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">{{heading}}</h1>
+          <div class="is-two-thirds column is-paddingless">
+            <h2 class="subtitle">{{subheading}}</h2>
+          </div>
+          <a href="" class="button is-large is-primary" id="learn">Learn more</a>
+        </div>
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="container">
+        <div class="columns pd is-desktop">
+          <div class="column is-1 has-text-centered">
+            <i class="fa fa-cog is-primary"></i>
+          </div>
+          <div class="column is-one-thirds-desktop">
+            <p class="title"><strong>We provide superior logistics</strong></p>
+          </div>
+          <div class="column">
+            <p><strong>Lorem ipsum dolor sit amet</strong></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="columns pd">
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">
+                I think it's an absolutly excellent
+              </p>
+              <p class="subtitle">- Gary Simon</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">
+                I think it's an absolutly excellent
+              </p>
+              <p class="subtitle">- Gary Simon</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">
+                I think it's an absolutly excellent
+              </p>
+              <p class="subtitle">- Gary Simon</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+    data: () => ({
+      heading: 'Soaring to new heights',
+      subheading: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    })
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+  @import '../mq'
+
+  .hero
+    background: url('../assets/clouds.jpg')
+    background-size: cover
+
+    .title
+      +mobile
+        font-weight: bold
+      +tablet
+        font-size: 2.5rem
+      +desktop
+        font-size: 4rem
+        marmgin-top: 2rem
+
+  .h2
+    margin: 1.5rem 0 2rem 0 !important
+
+  .fa-cog
+    font-size: 40px
+
+  #learn
+    +desktop
+      margin-bottom: 2rem
+
+  .pd
+    +tablet
+      padding: 2em 0
+
+
+</style>
